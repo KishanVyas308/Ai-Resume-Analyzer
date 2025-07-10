e7158fd22aa8d4c1cb4282fa378921def06ab3c1
Kishan Vyas <kishanvyas308@gmail.com>
2025-07-10 14:46:14 +0530

    frontend setup

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline bg-blue-500 text-white p-4">
+        Hello world!
+      </h1>
+
+    </div>
+  )
+}
+
+export default App
